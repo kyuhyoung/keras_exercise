c4332f18ddf3ce8a3ecdb751031115987da29d3d
Kyuhyoung Choi <kyuhyoung@gmail.com>
2019-01-18 02:39:36 +0900

    Fix keras_cnn_example

--- keras_cnn_example.py
+++ keras_cnn_example.py
@@ -16,24 +16,28 @@ print(X_train.shape)
 print(X_test.shape)
 print(y_train.shape)
 print(y_test.shape)
+
 #plt.imshow(X_train[0])
-X_train = X_train.reshape(X_train.shape[0], 1, 28, 28)
-print(X_train.shape)
+
+X_train = X_train.reshape(X_train.shape[0], 28, 28, 1)
+print("X_train.shape"); print(X_train.shape)
 #plt.imshow(X_train[0, 0])
-X_test = X_test.reshape(X_test.shape[0], 1, 28, 28)
+X_test = X_test.reshape(X_test.shape[0], 28, 28, 1)
+print("X_test.shape"); print(X_test.shape)
 
 X_train = X_train.astype('float32')
 X_test = X_test.astype('float32')
 X_train /= 255
 X_test /= 255
 
-print(y_train[:10])
+print("y_train[:10]");  print(y_train[:10])
 Y_train = np_utils.to_categorical(y_train, 10)
-print(Y_train.shape)
+print("Y_train.shape"); print(Y_train.shape)
 Y_test = np_utils.to_categorical(y_test, 10)
+print("Y_test.shape"); print(Y_test.shape)
 
 model = Sequential()
-model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(1,28,28)))
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
 print(model.output_shape)
 model.add(Conv2D(32, (3, 3), activation='relu'))
 print(model.output_shape)
@@ -53,10 +57,8 @@ print(model.output_shape)
 model.compile(loss='categorical_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
-print(model.input_shape)
-print(model.output_shape)
+print("model.input_shape");     print(model.input_shape)
+print("model.output_shape");    print(model.output_shape)
 
-model.fit(X_train, Y_train,
-          batch_size=2, epochs=10, verbose=2)
-dummy = 0
-
+model.fit(X_train, Y_train, batch_size=1000, epochs=10, verbose=1)
+print("Finished");
